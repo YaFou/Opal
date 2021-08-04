5581b62e28290cf7f3d59a4ddba558e31ae07de7
YaFou <33806646+YaFou@users.noreply.github.com>
2021-08-04 17:46:32 +0200

    finish scanner

--- src/scan.c
+++ src/scan.c
@@ -2,6 +2,8 @@
 #include "error.h"
 #include "util.h"
 #include <stdbool.h>
+#include "stringbuilder.h"
+#include <string.h>
 
 typedef struct {
     Module* module;
@@ -32,9 +34,14 @@ static Scanner* newScanner(Module* module)
     return scanner;
 }
 
+static char getCharAt(Scanner* scanner, int index)
+{
+    return scanner->module->source[index];
+}
+
 static char getChar(Scanner* scanner)
 {
-    return scanner->module->source[scanner->currentIndex];
+    return getCharAt(scanner, scanner->currentIndex);
 }
 
 static void advance(Scanner* scanner)
@@ -62,20 +69,54 @@ static void back(Scanner* scanner)
     scanner->currentIndex--;
 }
 
+static char getNextChar(Scanner* scanner)
+{
+    return getCharAt(scanner, scanner->currentIndex + 1);
+}
+
+static bool isCharMatch(Scanner* scanner, char c)
+{
+    if (isAtEnd(scanner) || getNextChar(scanner) != c) {
+        return false;
+    }
+
+    advance(scanner);
+
+    return true;
+}
+
 static Token* makeNumber(Scanner* scanner)
 {
     char c = getChar(scanner);
-    int value = convertCharToInteger(c);
+    float value = convertCharToInteger(c);
     advance(scanner);
+    bool hasDot = false;
+    int divider = 10;
 
     while (isDigit(c = getChar(scanner))) {
-        value = value * 10 + convertCharToInteger(c);
+        value = hasDot ?
+            value + convertCharToInteger(c) / divider :
+            value * 10 + convertCharToInteger(c);
+        
+        if (hasDot) {
+            divider *= 10;
+        }
+
+        if (!hasDot && isCharMatch(scanner, '.')) {
+            hasDot = true;
+        }
+
         advance(scanner);
     }
 
     back(scanner);
-    Token* token = makeToken(scanner, TOKEN_INTEGER);
-    token->value.integer = value;
+    Token* token = makeToken(scanner, hasDot ? TOKEN_FLOAT : TOKEN_INTEGER);
+    
+    if (hasDot) {
+        token->value._float = value;
+    } else {
+        token->value.integer = (int)value;
+    }
 
     return token;
 }
@@ -93,6 +134,155 @@ static void skipWhitespaces(Scanner* scanner)
     }
 }
 
+static bool isAlpha(char c)
+{
+    return (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        c == '_';
+}
+
+static Token* makeKeyword(Scanner* scanner, char* keyword)
+{
+    switch (*keyword) {
+        case 'a':
+            if (!strcmp(keyword, "abstract")) {
+                return makeToken(scanner, TOKEN_ABSTRACT);
+            }
+            break;
+        case 'b':
+            if (!strcmp(keyword, "break")) {
+                return makeToken(scanner, TOKEN_BREAK);
+            }
+            break;
+        case 'c':
+            if (!strcmp(keyword, "class")) {
+                return makeToken(scanner, TOKEN_CLASS);
+            }
+            if (!strcmp(keyword, "const")) {
+                return makeToken(scanner, TOKEN_CONST);
+            }
+            if (!strcmp(keyword, "continue")) {
+                return makeToken(scanner, TOKEN_CONTINUE);
+            }
+            break;
+        case 'd':
+            if (!strcmp(keyword, "do")) {
+                return makeToken(scanner, TOKEN_DO);
+            }
+            break;
+        case 'e':
+            if (!strcmp(keyword, "else")) {
+                return makeToken(scanner, TOKEN_ELSE);
+            }
+            if (!strcmp(keyword, "enum")) {
+                return makeToken(scanner, TOKEN_ENUM);
+            }
+            break;
+        case 'f':
+            if (!strcmp(keyword, "for")) {
+                return makeToken(scanner, TOKEN_FOR);
+            }
+            break;
+        case 'i':
+            if (!strcmp(keyword, "if")) {
+                return makeToken(scanner, TOKEN_IF);
+            }
+            if (!strcmp(keyword, "interface")) {
+                return makeToken(scanner, TOKEN_INTERFACE);
+            }
+            break;
+        case 'l':
+            if (!strcmp(keyword, "loop")) {
+                return makeToken(scanner, TOKEN_LOOP);
+            }
+            break;
+        case 'r':
+            if (!strcmp(keyword, "return")) {
+                return makeToken(scanner, TOKEN_RETURN);
+            }
+            break;
+        case 'v':
+            if (!strcmp(keyword, "var")) {
+                return makeToken(scanner, TOKEN_VAR);
+            }
+            break;
+        case 'w':
+            if (!strcmp(keyword, "while")) {
+                return makeToken(scanner, TOKEN_WHILE);
+            }
+            break;
+    }
+
+    return NULL;
+}
+
+static Token* makeIdentifier(Scanner* scanner)
+{
+    StringBuilder* builder = newStringBuilder();
+
+    while (isAlpha(getChar(scanner)) || isDigit(getChar(scanner))) {
+        addStringBuilder(builder, getChar(scanner));
+        advance(scanner);
+    }
+
+    back(scanner);
+
+    char* identifier = strdup(buildStringBuilder(builder));
+    freeStringBuilder(builder);
+    Token* token = makeKeyword(scanner, identifier);
+
+    if (token != NULL) {
+        return token;
+    }
+
+    token = makeToken(scanner, TOKEN_IDENTIFIER);
+    token->value.string = identifier;
+
+    return token;
+}
+
+static bool expectChar(Scanner* scanner, char c, char* message)
+{
+    if (!isCharMatch(scanner, c)) {
+        addErrorAt(scanner->module, scanner->currentIndex + 1, scanner->currentIndex + 2, message, getNextChar(scanner));
+    }
+}
+
+static Token* makeChar(Scanner* scanner)
+{
+    advance(scanner);
+    Token* token = makeToken(scanner, TOKEN_CHAR);
+    token->value.c = getChar(scanner);
+    expectChar(scanner, '\'', "A character value must be closed with \"'\" but received \"%c\".");
+
+    return token;
+}
+
+static Token* makeString(Scanner* scanner)
+{
+    advance(scanner);
+    StringBuilder* builder = newStringBuilder();
+    char c;
+
+    while (c = getChar(scanner) != '"') {
+        addStringBuilder(builder, c);
+        advance(scanner);
+
+        if (isAtEnd(scanner)) {
+            addErrorAt(scanner->module, scanner->currentIndex, scanner->currentIndex + 1, "A string value must be closed with '\"' but it's the end of the file.");
+            back(scanner);
+
+            return makeToken(scanner, TOKEN_NONE);
+        }
+    }
+
+    Token* token = makeToken(scanner, TOKEN_STRING);
+    token->value.string = strdup(buildStringBuilder(builder));
+    freeStringBuilder(builder);
+
+    return token;
+}
+
 static Token* getToken(Scanner* scanner)
 {
     skipWhitespaces(scanner);
@@ -103,25 +293,44 @@ static Token* getToken(Scanner* scanner)
 
     char c = getChar(scanner);
 
+    if (isAlpha(c)) {
+        return makeIdentifier(scanner);
+    }
+
     if (isDigit(c)) {
         return makeNumber(scanner);
     }
 
     switch (c) {
-        case '+': return makeToken(scanner, TOKEN_PLUS);
-        case '-': return makeToken(scanner, TOKEN_MINUS);
-        case '*': return makeToken(scanner, TOKEN_STAR);
-        case '/': return makeToken(scanner, TOKEN_SLASH);
+        case '+': return makeToken(scanner, isCharMatch(scanner, '=') ? TOKEN_PLUS_EQUAL : TOKEN_PLUS);
+        case '-': return makeToken(scanner, isCharMatch(scanner, '=') ? TOKEN_MINUS_EQUAL : TOKEN_MINUS);
+        case '*': return makeToken(scanner, isCharMatch(scanner, '=') ? TOKEN_STAR_EQUAL : TOKEN_STAR);
+        case '/': return makeToken(scanner, isCharMatch(scanner, '/') ? TOKEN_NONE : isCharMatch(scanner, '=') ? TOKEN_STAR_EQUAL : TOKEN_STAR);
         case '(': return makeToken(scanner, TOKEN_LEFT_PAREN);
         case ')': return makeToken(scanner, TOKEN_RIGHT_PAREN);
         case '{': return makeToken(scanner, TOKEN_LEFT_BRACE);
         case '}': return makeToken(scanner, TOKEN_RIGHT_BRACE);
         case ';': return makeToken(scanner, TOKEN_SEMILICON);
+        case '=': return makeToken(scanner, isCharMatch(scanner, '=') ? TOKEN_DOUBLE_EQUAL : TOKEN_EQUAL);
+        case '!': return makeToken(scanner, isCharMatch(scanner, '=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        case '<': return makeToken(scanner, isCharMatch(scanner, '=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        case '>': return makeToken(scanner, isCharMatch(scanner, '=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+        case '.': return makeToken(scanner, TOKEN_DOT);
+        case ',': return makeToken(scanner, TOKEN_COMMA);
+        case '^': return makeToken(scanner, TOKEN_CIRCUMFLEX);
+        case '&': return makeToken(scanner, expectChar(scanner, '&', "Expect \"&&\" but received \"&%c\".") ? TOKEN_DOUBLE_AMPERSAND : TOKEN_NONE);
+        case '|': return makeToken(scanner, expectChar(scanner, '|', "Expect \"||\" but received \"|%c\".") ? TOKEN_DOUBLE_PIPE : TOKEN_NONE);
+        case '?': return makeToken(scanner, TOKEN_QUESTION_MARK);
+        case '%': return makeToken(scanner, isCharMatch(scanner, '=') ? TOKEN_MODULO_EQUAL : TOKEN_MODULO);
+        case '[': return makeToken(scanner, TOKEN_LEFT_BRACKET);
+        case ']': return makeToken(scanner, TOKEN_RIGHT_BRACKET);
+        case '\'': return makeChar(scanner);
+        case '"': return makeString(scanner);
     }
 
     addErrorAt(scanner->module, scanner->currentIndex, scanner->currentIndex + 1, "Unexpected character \"%c\".", c);
 
-    return makeToken(scanner, TOKEN_ERROR);
+    return makeToken(scanner, TOKEN_NONE);
 }
 
 Vector* scan(Module* module)
@@ -133,7 +342,7 @@ Vector* scan(Module* module)
         advance(scanner);
         scanner->startIndex = scanner->currentIndex;
 
-        if (token->type == TOKEN_ERROR || hasErrors()) {
+        if (token->type == TOKEN_NONE || hasErrors()) {
             if (token->type == TOKEN_EOF) {
                 break;
             }
@@ -147,9 +356,7 @@ Vector* scan(Module* module)
             break;
         }
     }
-
-    // Token* eofToken = makeToken(scanner, TOKEN_EOF);
-    // pushVector(scanner->tokens, eofToken);
+    
     Vector* tokens = scanner->tokens;
     free(scanner);
 
--- src/scan.h
+++ src/scan.h
@@ -6,28 +6,74 @@
 
 typedef enum {
     // SIGNS
-    TOKEN_PLUS,         // +
-    TOKEN_MINUS,        // -
-    TOKEN_STAR,         // *
-    TOKEN_SLASH,        // /
-    TOKEN_LEFT_PAREN,   // (
-    TOKEN_RIGHT_PAREN,  // )
-    TOKEN_LEFT_BRACE,   // {
-    TOKEN_RIGHT_BRACE,  // }
-    TOKEN_SEMILICON,    // ;
+    TOKEN_PLUS,             // +
+    TOKEN_MINUS,            // -
+    TOKEN_STAR,             // *
+    TOKEN_SLASH,            // /
+    TOKEN_LEFT_PAREN,       // (
+    TOKEN_RIGHT_PAREN,      // )
+    TOKEN_LEFT_BRACE,       // {
+    TOKEN_RIGHT_BRACE,      // }
+    TOKEN_SEMILICON,        // ;
+    TOKEN_EQUAL,            // =
+    TOKEN_DOUBLE_EQUAL,     // ==
+    TOKEN_BANG_EQUAL,       // !=
+    TOKEN_BANG,             // !
+    TOKEN_LESS,             // <
+    TOKEN_LESS_EQUAL,       // <=
+    TOKEN_GREATER,          // >
+    TOKEN_GREATER_EQUAL,    // >=
+    TOKEN_DOT,              // .
+    TOKEN_COMMA,            // ,
+    TOKEN_CIRCUMFLEX,       // ^
+    TOKEN_DOUBLE_AMPERSAND, // &&
+    TOKEN_DOUBLE_PIPE,      // ||
+    TOKEN_QUESTION_MARK,    // ?
+    TOKEN_PLUS_EQUAL,       // +=
+    TOKEN_MINUS_EQUAL,      // -=
+    TOKEN_STAR_EQUAL,       // *=
+    TOKEN_SLASH_EQUAL,      // /=
+    TOKEN_MODULO,           // %
+    TOKEN_MODULO_EQUAL,     // %=
+    TOKEN_LEFT_BRACKET,       // [
+    TOKEN_RIGHT_BRACKET,      // ]
 
     // VALUES
     TOKEN_INTEGER,
+    TOKEN_STRING,
+    TOKEN_CHAR,
+    TOKEN_FLOAT,
+    TOKEN_IDENTIFIER,
+
+    // KEYWORDS
+    TOKEN_ABSTRACT,
+    TOKEN_BREAK,
+    TOKEN_CLASS,
+    TOKEN_CONST,
+    TOKEN_CONTINUE,
+    TOKEN_DO,
+    TOKEN_ELSE,
+    TOKEN_ENUM,
+    TOKEN_FOR,
+    TOKEN_IF,
+    TOKEN_INTERFACE,
+    TOKEN_LOOP,
+    TOKEN_RETURN,
+    TOKEN_VAR,
+    TOKEN_WHILE,
 
     // OTHERS
     TOKEN_EOF,
-    TOKEN_ERROR
+    TOKEN_NONE
 } TokenType;
 
 typedef struct {
     TokenType type;
     union {
         int integer;
+        char c;
+        float _float;
+        char* string;
     } value;
 
     Module* module;
